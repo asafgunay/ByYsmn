d6c3f2581ebf3fb25acfc2a41a5ffda8a4e2c994
asafgunay <asafgunay@gmail.com>
2019-06-29 16:24:37 +0300

    company service create method created

--- src/ByYsmn.Application/CompanyServices/CompanyService.cs
+++ src/ByYsmn.Application/CompanyServices/CompanyService.cs
@@ -5,19 +5,16 @@ using System.Threading.Tasks;
 using ByYsmn.Application.CompanyServices.Dtos;
 using ByYsmn.Application.Shared;
 using ByYsmn.Core.Companies;
+using ByYsmn.Entityframework.Contexts;
 
 namespace ByYsmn.Application.CompanyServices
 {
     public class CompanyService : ICompanyService
     {
-        public Task<Company> Create(CompanyCreateInput input)
+        private readonly ApplicationDbContext _context;
+        public CompanyService(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> Delete(EntityInput<Guid> input)
-        {
-            throw new NotImplementedException();
+            _context = context;
         }
 
         public Task<Company> Get(EntityInput<Guid> input)
@@ -40,7 +37,18 @@ namespace ByYsmn.Application.CompanyServices
             throw new NotImplementedException();
         }
 
+        public async Task<Company> Create(CompanyCreateInput input)
+        {
+            var company = Company.Create(input.Name, input.Tel, input.Address, input.WebSiteUrl, input.Email, input.CreatorUserId);
+            await _context.Companies.AddAsync(company);
+            await _context.SaveChangesAsync();
+            return company;
+        }
         public Task<Company> Update(CompanyUpdateInput input)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> Delete(EntityInput<Guid> input)
         {
             throw new NotImplementedException();
         }
--- src/ByYsmn.Core/Companies/Company.cs
+++ src/ByYsmn.Core/Companies/Company.cs
@@ -14,6 +14,33 @@ namespace ByYsmn.Core.Companies
         public string Address { get; set; }
         public string WebSiteUrl { get; set; }
         public string Email { get; set; }
-
+        public static Company Create(string name, string tel, string address, string webSiteUrl, string email, Guid? creatorUserId)
+        {
+            return new Company
+            {
+                Name = name,
+                Tel = tel,
+                Address = address,
+                WebSiteUrl = webSiteUrl,
+                Email = email,
+                CreatorUserId = creatorUserId,
+                CreatedDate = DateTime.Now
+            };
+        }
+        public static Company Update(Company oldCompany, string name, string tel, string address, string webSiteUrl, string email, Guid? modifiedUserId)
+        {
+            return new Company
+            {
+                Id = oldCompany.Id,
+                CreatedDate = oldCompany.CreatedDate,
+                CreatorUserId = oldCompany.CreatorUserId,
+                Name = name,
+                Tel = tel,
+                Address = address,
+                WebSiteUrl = webSiteUrl,
+                Email = email,
+                ModifierUserId = modifiedUserId
+            };
+        }
     }
 }
