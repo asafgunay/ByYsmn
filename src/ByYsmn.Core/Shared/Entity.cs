76d7413ac2930ebb0b505f4225f7636030a528cc
Asaf Günay <asaf.gunay@windowslive.com>
2019-06-29 12:47:28 +0300

    ApplicationUser ID type set as Guid Entity base classes created first service begin

---
+++ src/ByYsmn.Core/Shared/Entity.cs
@@ -0,0 +1,17 @@
+﻿using ByYsmn.Core.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ByYsmn.Core.Shared
+{
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+        [ForeignKey("CreatorUserId")]
+        public virtual ApplicationUser CreatorUser { get; set; }
+        public virtual Guid CreatorUserId { get; set; }
+        public string ModifierUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ src/ByYsmn.Entityframework/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ByYsmn.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ByYsmn.Entityframework.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        // DbSetler buraya eklenecek.(Veritabani Modeller)
+    }
+}
